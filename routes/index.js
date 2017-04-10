3d08d2a57a22e5293cacc5bb2bc66968030476bd
Khai Phan <kphan95@bu.edu>
2017-04-10 12:39:55 -0400

    Incorporate Skeleton. Added landing & Construction Page

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,24 @@ router.get('/', function (req, res, next) {
   return res.render('landing')
 })
 
+router.get('/home', function (req, res, next) {
+  return res.redirect('/')
+})
+
+router.get('/resume', function (req, res, next) {
+  return res.redirect('/files/Khai_Phan_Resume.pdf')
+})
+
+router.get('/projects', function (req, res, next) {
+  return res.render('construction')
+})
+
+router.get('/gallery', function (req, res, next) {
+  return res.render('construction')
+})
+
+router.get('/aboutme', function (req, res, next) {
+  return res.render('construction')
+})
+
 module.exports = router
